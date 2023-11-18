3d1360c6d6b0338b218c9abb890948684051619f
Mose Mueller <mosemueller@gmail.com>
2023-11-18 10:45:40 +0100

    observer constructor takes observable

--- src/observable_pattern/observer.py
+++ src/observable_pattern/observer.py
@@ -1,10 +1,16 @@
 from abc import abstractmethod
 from typing import Any
 
+from observable_pattern.observable import Observable
 from observable_pattern.observable_object import ObservableObject
 
 
 class Observer(ObservableObject):
+    def __init__(self, observable: Observable) -> None:
+        super().__init__()
+        self.observable = observable
+        self.observable.add_observer(self)
+
     def _notify_observers(self, changed_attribute: str, value: Any) -> None:
         self.on_change(full_access_path=changed_attribute, value=value)
 
