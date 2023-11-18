fc4da8e525408b2cade99f0852a5c22b5ddfe56e
Mose Mueller <mosemueller@gmail.com>
2023-11-18 10:45:40 +0100

    adds dictionary tests

--- tests/test_observable_object.py
+++ tests/test_observable_object.py
@@ -70,6 +70,66 @@ def test_class_object_list_attribute(caplog: pytest.LogCaptureFixture) -> None:
     assert "'list_attr[0].name' changed to 'Ciao'" in caplog.text  # noqa: S101
 
 
+def test_simple_instance_dict_attribute(caplog: pytest.LogCaptureFixture) -> None:
+    class MyObservable(observable_pattern.Observable):
+        def __init__(self) -> None:
+            super().__init__()
+            self.dict_attr = {"first": "Hello"}
+
+    instance = MyObservable()
+    instance.add_observer(MyObserver())
+    instance.dict_attr["first"] = "Ciao"
+    instance.dict_attr["second"] = "World"
+
+    assert "'dict_attr[first]' changed to 'Ciao'" in caplog.text  # noqa: S101
+    assert "'dict_attr[second]' changed to 'World'" in caplog.text  # noqa: S101
+
+
+def test_simple_class_dict_attribute(caplog: pytest.LogCaptureFixture) -> None:
+    class MyObservable(observable_pattern.Observable):
+        dict_attr = {"first": "Hello"}
+
+    instance = MyObservable()
+    instance.add_observer(MyObserver())
+    instance.dict_attr["first"] = "Ciao"
+    instance.dict_attr["second"] = "World"
+
+    assert "'dict_attr[first]' changed to 'Ciao'" in caplog.text  # noqa: S101
+    assert "'dict_attr[second]' changed to 'World'" in caplog.text  # noqa: S101
+
+
+def test_instance_dict_attribute(caplog: pytest.LogCaptureFixture) -> None:
+    class NestedObservable(observable_pattern.Observable):
+        def __init__(self) -> None:
+            super().__init__()
+            self.name = "Hello"
+
+    class MyObservable(observable_pattern.Observable):
+        def __init__(self) -> None:
+            super().__init__()
+            self.dict_attr = {"first": NestedObservable()}
+
+    instance = MyObservable()
+    instance.add_observer(MyObserver())
+    instance.dict_attr["first"].name = "Ciao"
+
+    assert "'dict_attr[first].name' changed to 'Ciao'" in caplog.text  # noqa: S101
+
+
+def test_class_dict_attribute(caplog: pytest.LogCaptureFixture) -> None:
+    class NestedObservable(observable_pattern.Observable):
+        name = "Hello"
+
+    class MyObservable(observable_pattern.Observable):
+        dict_attr = {"first": NestedObservable()}
+
+    instance = MyObservable()
+    instance.add_observer(MyObserver())
+    instance.dict_attr["first"].name = "Ciao"
+
+    assert "'dict_attr[first].name' changed to 'Ciao'" in caplog.text  # noqa: S101
+
+
 def test_removed_observer_on_class_attr(caplog: pytest.LogCaptureFixture) -> None:
     class NestedObservable(observable_pattern.Observable):
         name = "Hello"
@@ -114,6 +174,29 @@ def test_removed_observer_on_class_list_attr(caplog: pytest.LogCaptureFixture) -
 
     assert "'nested_attr.name' changed to 'Hi'" in caplog.text
     assert "'changed_list_attr[0].name' changed to 'Hi'" not in caplog.text
+
+
+def test_removed_observer_on_class_dict_attr(caplog: pytest.LogCaptureFixture) -> None:
+    class NestedObservable(observable_pattern.Observable):
+        name = "Hello"
+
+    nested_instance = NestedObservable()
+
+    class MyObservable(observable_pattern.Observable):
+        nested_attr = nested_instance
+        changed_dict_attr = {"nested": nested_instance}
+
+    instance = MyObservable()
+    instance.add_observer(MyObserver())
+    instance.changed_dict_attr["nested"] = "Ciao"
+
+    assert "'changed_dict_attr[nested]' changed to 'Ciao'" in caplog.text  # noqa: S101
+    caplog.clear()
+
+    instance.nested_attr.name = "Hi"
+
+    assert "'nested_attr.name' changed to 'Hi'" in caplog.text
+    assert "'changed_dict_attr[nested].name' changed to 'Hi'" not in caplog.text
 
 
 def test_removed_observer_on_instance_attr(caplog: pytest.LogCaptureFixture) -> None:
@@ -170,3 +253,30 @@ def test_removed_observer_on_instance_list_attr(
 
     assert "'nested_attr.name' changed to 'Hi'" in caplog.text
     assert "'changed_list_attr[0].name' changed to 'Hi'" not in caplog.text
+
+
+def test_removed_observer_on_class_dict_attr(caplog: pytest.LogCaptureFixture) -> None:
+    class NestedObservable(observable_pattern.Observable):
+        def __init__(self) -> None:
+            super().__init__()
+            self.name = "Hello"
+
+    nested_instance = NestedObservable()
+
+    class MyObservable(observable_pattern.Observable):
+        def __init__(self) -> None:
+            super().__init__()
+            self.nested_attr = nested_instance
+            self.changed_dict_attr = {"nested": nested_instance}
+
+    instance = MyObservable()
+    instance.add_observer(MyObserver())
+    instance.changed_dict_attr["nested"] = "Ciao"
+
+    assert "'changed_dict_attr[nested]' changed to 'Ciao'" in caplog.text  # noqa: S101
+    caplog.clear()
+
+    instance.nested_attr.name = "Hi"
+
+    assert "'nested_attr.name' changed to 'Hi'" in caplog.text
+    assert "'changed_dict_attr[nested].name' changed to 'Hi'" not in caplog.text
