4a48ea9b8ce3640838ff9ca1b22d3a015785493c
Mose Mueller <mosemueller@gmail.com>
2023-11-18 10:45:40 +0100

    moves observer into separate module

--- src/observable_pattern/__init__.py
+++ src/observable_pattern/__init__.py
@@ -1,8 +1,12 @@
 import logging
 
 from observable_pattern.observable import Observable
+from observable_pattern.observer import Observer
 from observable_pattern.utils.logging import setup_logging
 
 setup_logging(logging.DEBUG)
 
-__all__ = ["Observable"]
+__all__ = [
+    "Observable",
+    "Observer",
+]
---
+++ src/observable_pattern/observer/__init__.py
@@ -0,0 +1,3 @@
+from observable_pattern.observer.observer import Observer
+
+__all__ = ["Observer"]
