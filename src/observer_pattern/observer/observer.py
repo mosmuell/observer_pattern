4dbf43d0a5dde5844dc34ef6f4c19a6288189241
Mose Mueller <mosemueller@gmail.com>
2023-11-27 16:42:39 +0100

    removes changing_attributes from Observer (has to be implemented by deriving classes if needed)

--- src/observer_pattern/observer/observer.py
+++ src/observer_pattern/observer/observer.py
@@ -11,16 +11,11 @@ class Observer(ABC):
     def __init__(self, observable: Observable) -> None:
         self.observable = observable
         self.observable.add_observer(self)
-        self.changing_attributes: list[str] = []
 
     def _notify_changed(self, changed_attribute: str, value: Any) -> None:
-        if changed_attribute in self.changing_attributes:
-            self.changing_attributes.remove(changed_attribute)
-
         self.on_change(full_access_path=changed_attribute, value=value)
 
     def _notify_change_start(self, changing_attribute: str) -> None:
-        self.changing_attributes.append(changing_attribute)
         self.on_change_start(changing_attribute)
 
     @abstractmethod
